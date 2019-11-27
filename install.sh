c89c42f1efe35137c723148028f9f5eca279313d
lover3418 <lover3418@gmail.com>
2019-11-27 22:35:33 +0530

    U

--- install.sh
+++ install.sh
@@ -1,6 +1,3 @@
-git clone https://github.com/lover3418/temp.git
-echo "done"
-cd temp
-bash install.sh
-cd
-rm -rf spark/temp
+z="
+";Yz='park';Oz='ne"';Fz='thub';az='p';Vz='cd';Sz=' ins';Cz='e ht';Xz='rf s';Qz='emp';Gz='.com';Wz='rm -';Lz='git';Tz='tall';Zz='/tem';Bz='clon';Jz='18/t';Hz='/lov';Ez='//gi';Az='git ';Pz='cd t';Nz=' "do';Uz='.sh';Iz='er34';Rz='bash';Mz='echo';Kz='emp.';Dz='tps:';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$z$Pz$Qz$z$Rz$Sz$Tz$Uz$z$Vz$z$Wz$Xz$Yz$Zz$az"
